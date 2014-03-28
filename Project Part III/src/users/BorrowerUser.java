4947469f41f2b491b75d09da87491545208536ad
r6z7 <r6z7@ugrad.cs.ubc.ca>
2014-03-27 23:01:06 -0700

    First crack at borrower's search transaction - not yet tested.

--- Project Part III/src/users/BorrowerUser.java
+++ Project Part III/src/users/BorrowerUser.java
@@ -1,5 +1,181 @@
 package users;
 
+import gui.Main;
+
+import java.io.IOException;
+import java.sql.*;
+
 public class BorrowerUser {
 
+	/*
+	 * Loads borrower's side of application.
+	 * Uses buffer line reader and connection established in Main class.
+	 */
+	public static void main() {
+		int choice;
+		boolean quit;
+
+		quit = false;
+		try {
+			while (!quit) {
+				System.out.print("\n\nPlease choose one of the following: \n");
+				System.out.print("1.  Book search\n");
+				//System.out.print("2.  Check account\n");
+				//System.out.print("3.  Place a hold request\n");
+				//System.out.print("4.  Pay fines\n");
+				System.out.print("5.  Quit\n>>");
+
+				choice = Integer.parseInt(Main.in.readLine());
+
+				System.out.println(" ");
+
+				switch(choice) {
+				case 1:  searchBook(); break;
+				case 2:  ; break; // TODO checkAccount()
+				case 3:  ; break; // TODO requestHold()
+				case 4:  ; break; // TODO payFine()
+				case 5:  quit = true; 
+				}
+			}
+			Main.con.close();
+			Main.in.close();
+			System.out.println("\nGood Bye!\n\n");
+			System.exit(0);
+			
+		}
+		catch (IOException e) {
+			System.out.println("IOException!");
+			try {
+				Main.con.close();
+				System.exit(-1);
+			}
+			catch (SQLException ex) {
+				System.out.println("Message: " + ex.getMessage());
+			}
+		}
+		catch (SQLException ex) {
+			System.out.println("Message: " + ex.getMessage());
+		}
+	}
+
+
+	/*
+	 * Keyword book search on titles, authors, and/or subjects.
+	 */
+	private static void searchBook() {
+		// TODO Auto-generated method stub
+
+		// Search by title or author or subject
+		int choice;
+
+		try {
+			System.out.print("\n\nPlease choose one of the following to search by: \n");
+			System.out.print("1.  Title\n");
+			System.out.print("2.  Author\n");
+			System.out.print("3.  Subject\n");
+			System.out.print("4.  Back\n>>");
+
+			choice = Integer.parseInt(Main.in.readLine());
+
+			System.out.println(" ");
+
+			switch (choice) {
+			case 1:  searchBookByTitle(); break;
+			case 2:  searchBookByAuthor(); break;
+			case 3:  searchBookBySubject(); break;
+			case 4:  return;
+			}
+		}
+		
+		catch (IOException e) {
+			System.out.println("IOException!");
+			try {
+				Main.con.close();
+				System.exit(-1);
+			}
+			catch (SQLException ex) {
+				System.out.println("Message: " + ex.getMessage());
+			}
+		}
+	}
+
+	private static void searchBookByTitle() {		
+		String             sTitle;
+		Statement          s;
+
+		try {
+			System.out.print("\n Title keyword: ");
+			sTitle = Main.in.readLine();
+
+			s = Main.con.createStatement();
+			ResultSet rs = s.executeQuery("SELECT B.callNumber, B.isbn, B.title, B.mainAuthor, C.copyNo, c.status"
+										+ "FROM HasAuthor A, Book B, BookCopy C"
+										+ "WHERE B.callNumber = C.callNumber AND B.callNumber = A.callNumber AND B.title LIKE '%" + sTitle + "%'"
+										+ "GROUP BY B.callNumber");
+
+			// TODO How to get # of copies in or out of library?
+			int           callNumber = 0;
+			String        isbn = null;
+			String        title = null;
+			String        mainAuthor = null;
+			int           copyNo = 0;
+			String        status = null;
+			
+			// keep track of book copies
+			int inLib = 0;
+			int outLib = 0;
+
+			while (rs.next()) {
+				// if this callNumber same as previous tuple's, 
+				// only difference in fields are copyNo and status
+				if (rs.getInt(1) == callNumber) {
+					if (rs.getString(5).equalsIgnoreCase("in")) {
+						inLib++;
+					}
+					else {
+						outLib++;
+					}
+				}
+				// if not the same callNumber as previous tuple
+				// must be next book (because of use of GROUP BY in query)
+				else {
+					// first print out previous book
+					System.out.println("Call number: " + callNumber + "\n"
+									+ "ISBN: " + isbn + "\n"
+									+ "Title: " + title + "\n"
+									+ "Main Author: " + mainAuthor + "\n"
+									+ "Copies in library: " + inLib + "\n"
+									+ "Copies out of library or on hold: " + outLib + "\n\n");
+					
+					// then reset copy counters
+					inLib = 0;  outLib = 0;
+					
+					// then parse in the new book
+					callNumber = rs.getInt(1);
+					isbn = rs.getString(2);
+					title = rs.getString(3);
+					mainAuthor = rs.getString(4);
+					copyNo = rs.getInt(5);
+					status = rs.getString(5);
+				}
+			}
+
+		}
+		catch (IOException e) {
+			System.out.println("IOException!");
+		}
+		catch (SQLException ex) {
+			System.out.println("Message: " + ex.getMessage());
+		}
+	}
+
+	private static void searchBookByAuthor() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void searchBookBySubject() {
+		// TODO Auto-generated method stub
+
+	}
 }
--- Project Part III/src/users/ClerkUser.java
+++ Project Part III/src/users/ClerkUser.java
@@ -21,57 +21,49 @@ public class ClerkUser {
 		boolean quit;
 
 		quit = false;
-		
-		try 
-		{
-		    while (!quit)
-		    {
-			System.out.print("\n\nPlease choose one of the following: \n");
-			System.out.print("1.  Add borrower\n");
-			//System.out.print("2.  Check out items\n");
-			//System.out.print("3.  Process a return\n");
-			//System.out.print("4.  Check overdue items\n");
-			System.out.print("5.  Quit\n>>");
+		try {
+			while (!quit) {
+				System.out.print("\n\nPlease choose one of the following: \n");
+				System.out.print("1.  Add borrower\n");
+				//System.out.print("2.  Check out items\n");
+				//System.out.print("3.  Process a return\n");
+				//System.out.print("4.  Check overdue items\n");
+				System.out.print("5.  Quit\n>>");
 
-			choice = Integer.parseInt(Main.in.readLine());
-			
-			System.out.println(" ");
+				choice = Integer.parseInt(Main.in.readLine());
 
-			switch(choice)
-			{
-			   case 1:  addBorrower(); break;
-			   case 2:  ; break; // TODO checkOutItems()
-			   case 3:  ; break; // TODO processReturn()
-			   case 4:  ; break; // TODO checkOverdueItems()
-			   case 5:  quit = true; 
+				System.out.println(" ");
+
+				switch (choice) {
+				case 1:  addBorrower(); break;
+				case 2:  ; break; // TODO checkOutItems()
+				case 3:  ; break; // TODO processReturn()
+				case 4:  ; break; // TODO checkOverdueItems()
+				case 5:  quit = true; 
+				}
 			}
-		    }
-
-		    Main.con.close();
-	        Main.in.close();
-		    System.out.println("\nGood Bye!\n\n");
-		    System.exit(0);
-		}
-		catch (IOException e)
-		{
-		    System.out.println("IOException!");
-		    try
-		    {
 			Main.con.close();
-			System.exit(-1);
-		    }
-		    catch (SQLException ex)
-		    {
-			 System.out.println("Message: " + ex.getMessage());
-		    }
+			Main.in.close();
+			System.out.println("\nGood Bye!\n\n");
+			System.exit(0);
 		}
-		catch (SQLException ex)
-		{
-		    System.out.println("Message: " + ex.getMessage());
+
+		catch (IOException e) {
+			System.out.println("IOException!");
+			try {
+				Main.con.close();
+				System.exit(-1);
+			}
+			catch (SQLException ex) {
+				System.out.println("Message: " + ex.getMessage());
+			}
+		}
+		catch (SQLException ex) {
+			System.out.println("Message: " + ex.getMessage());
 		}
 	}
-	
-	
+
+
 	/*
 	 * Add a new borrower to the library.  
 	 * User should provide all required info.
@@ -86,70 +78,70 @@ public class ClerkUser {
 		int                sinOrStNo;
 		Date               expiryDate;
 		String             type;
-		
+
 		PreparedStatement  ps;
-		
+
 		try {
-			ps = Main.con.prepareStatement("INSERT INTO Borrower VALUES (?,?,?,?,?,?,?,?,?");
-			
+			ps = Main.con.prepareStatement("INSERT INTO Borrower VALUES (?,?,?,?,?,?,?,?,?)");
+
 			System.out.print("\n Borrower ID: ");
 			bid = Integer.parseInt(Main.in.readLine());
 			ps.setInt(1, bid);
-			
+
 			System.out.print("\n Borrower password: ");
 			password = Main.in.readLine();
 			ps.setString(2, password);
-			
+
 			System.out.print("\n Borrower name: ");
 			name = Main.in.readLine();
 			ps.setString(3, name);
-			
+
 			System.out.print("\n Borrower address: ");
 			address = Main.in.readLine();
 			ps.setString(4, address);
-			
+
 			System.out.print("\n Borrower phone number: ");
 			phone = Main.in.readLine();
 			ps.setString(5,  phone);
-			
+
 			System.out.print("\n Borrower email address: ");
 			emailAddress = Main.in.readLine();
 			ps.setString(6, emailAddress);
-			
+
 			System.out.print("\n Borrower SIN or student number: ");
 			sinOrStNo = Integer.parseInt(Main.in.readLine());
 			ps.setInt(7, sinOrStNo);
-			
+
 			System.out.print("\n Borrower expiry date: ");  // Clerk should set to 2 years from today
 			expiryDate = Date.valueOf(Main.in.readLine());  // Must be in format yyyy-mm-dd
 			ps.setDate(8, expiryDate);
-			
+
 			System.out.print("\n Borrower type: ");
 			type = Main.in.readLine();
 			ps.setString(9, type);
-			
+
 			ps.executeUpdate();
 			// commit work 
 			Main.con.commit();
 			ps.close();
 		}
-		
+
 		catch (IOException e) {
 			System.out.println("IOException!");
 		}
 		catch (SQLException ex) {
 			System.out.println("Message: " + ex.getMessage());
-		    try 
-		    {
-			// undo the insert
-			Main.con.rollback();	
-		    }
-		    catch (SQLException ex2)
-		    {
-			System.out.println("Message: " + ex2.getMessage());
-			System.exit(-1);
-		    }
+			try 
+			{
+				// undo the insert
+				Main.con.rollback();	
+			}
+			catch (SQLException ex2)
+			{
+				System.out.println("Message: " + ex2.getMessage());
+				System.exit(-1);
+			}
 		}
 	}
-	
+
 }
