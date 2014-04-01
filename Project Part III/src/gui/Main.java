d64d03c5e8821f2fa49155ae7d4ce1afbbee43a2
kbhangel <gwanggo1@hotmail.com>
2014-04-01 05:06:22 -0700

    checkOverdue txtarea commented

--- Project Part III/src/gui/Main.java
+++ Project Part III/src/gui/Main.java
@@ -1291,7 +1291,7 @@ public class Main implements ActionListener {
 			panelcheckOverdueItemsSouth.add(emailcheckOverdueItems);
 			toppanelcheckOverdueItems.add(panelcheckOverdueItemsNorth);
 			toppanelcheckOverdueItems.add(scrollPaneCheckOverdue);
-			toppanelcheckOverdueItems.add(checkOverdueItemstxtarea);
+//			toppanelcheckOverdueItems.add(checkOverdueItemstxtarea);
 			toppanelcheckOverdueItems.add(panelcheckOverdueItemsSouth);
 			
 		//add listeners to the buttons
